74a148e48f24604c64c6a72263d2a49437ee4124
hyunduk <kkang13277@naver.com>
2023-05-01 16:44:24 +0900

    유저 정보 받는 메서드 추가

--- src/main/java/com/gumi/enjoytrip/domain/user/repository/UserRepository.java
+++ src/main/java/com/gumi/enjoytrip/domain/user/repository/UserRepository.java
@@ -7,4 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String username);
+
+    Optional<User> findByNickname(String nickname);
+
+    User getUser(long userId);
 }
